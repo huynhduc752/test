1e998f4191c0780bd67a2cf6bfe587238831a112
huynhduc752 <huynhduc752@gmail.com>
2023-04-19 11:02:15 +0700

    test

---
+++ testFireBase/testFireBase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testFireBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
